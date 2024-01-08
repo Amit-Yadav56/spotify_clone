afa9a743f6a84640c41e4b38c37be0727300000c
Amit <bagi.mukesh2001@gmail.com>
2024-01-08 15:36:19 +0530

    updated website

--- public/index.js
+++ public/index.js
@@ -10,16 +10,34 @@ async function getSongs(){
     for (let index = 0; index < as.length; index++) {
         const element = as[index];
         if(element.href.endsWith(".mp3")){
-            songs.push(element.href);
+            songs.push(element.href.split("/songs/")[1]);
         }
     }
     return songs;
 }
 async function main(){
     let songs =await getSongs();
+    console.log(songs)
+    let songUL=document.querySelector(".songlist").getElementsByTagName("ul")[0]
+    for(const song of songs){
+        songUL.innerHTML = songUL.innerHTML + `<li class="flex">
+                            <img src="/images/music.svg" class="invert" alt="">
+                            <div class="info">
+                                <div > ${song.replaceAll("%20", " ")} </div>
+                                <div >ME</div>
+                            </div>
+                            <div class="playNow">
+                                <span>Play Now</span>
+                                <img src="/images/play-song.svg" class="invert" alt="">
+                                
+                            </div>
+                        </li>`;
+    }
+
     var audio = new Audio(songs[0]);
     audio.play();
     audio.addEventListener("loadeddata",()=>{
         console.log(audio.duration,audio.currentSrc,audio.currentTime);
     })
 }
+main();
