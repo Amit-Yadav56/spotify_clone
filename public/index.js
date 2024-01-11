e29444962b039a53f0c193f6104dfaa2107dd707
Amit <bagi.mukesh2001@gmail.com>
2024-01-12 00:35:55 +0530

    Made the website responsive

--- public/index.js
+++ public/index.js
@@ -1,4 +1,17 @@
 console.log("js")
+let currentSong=new Audio();
+function secondsToMinutesAndSeconds(seconds) {
+    const minutes = Math.floor(seconds / 60);
+    const remainingSeconds = Math.floor(seconds % 60);
+
+    // Ensure that both minutes and seconds have two digits
+    const formattedMinutes = String(minutes).padStart(2, '0');
+    const formattedSeconds = String(remainingSeconds).padStart(2, '0');
+
+    return `${formattedMinutes}:${formattedSeconds}`;
+}
+
+
 async function getSongs(){
     let songs = [];
     let a= await fetch("http://127.0.0.1:5500/songs/");
@@ -15,9 +28,23 @@ async function getSongs(){
     }
     return songs;
 }
+const playMusic=(track,pause=false)=>{
+    // let audio=new Audio("/songs/"+track)
+    currentSong.src="/songs/"+track
+    if(!pause){
+        currentSong.play()
+        playsong.src="/images/pause.svg"
+    }
+    document.querySelector(".songinfo").innerHTML=decodeURI(track)
+    document.querySelector(".songtime").innerHTML=`00:00`
+
+}
 async function main(){
+    
+    // get list of all songs
     let songs =await getSongs();
-    console.log(songs)
+    playMusic(songs[0],true)
+    // show all songs on playlist
     let songUL=document.querySelector(".songlist").getElementsByTagName("ul")[0]
     for(const song of songs){
         songUL.innerHTML = songUL.innerHTML + `<li class="flex">
@@ -33,11 +60,46 @@ async function main(){
                             </div>
                         </li>`;
     }
+    //attach an event listener to each song
+    Array.from(document.querySelector(".songlist").getElementsByTagName('li')).forEach( e=> {
+        e.addEventListener("click",element=>{
+            console.log(e.querySelector(".info").firstElementChild.innerHTML); 
+            playMusic(e.querySelector(".info").firstElementChild.innerHTML.trim());
 
-    var audio = new Audio(songs[0]);
-    audio.play();
-    audio.addEventListener("loadeddata",()=>{
-        console.log(audio.duration,audio.currentSrc,audio.currentTime);
+        });
+    });
+
+    //Attach a event listener for prev play next
+    playsong.addEventListener("click",()=>{
+        if(currentSong.paused){
+            currentSong.play()
+            playsong.src="/images/pause.svg"
+        }
+        else{
+            currentSong.pause()
+            playsong.src="/images/play-song.svg"
+        }
+    })
+
+    //listen for  timeupdate event
+        currentSong.addEventListener("timeupdate", ()=>{
+            document.querySelector(".songtime").innerHTML=`${secondsToMinutesAndSeconds(currentSong.currentTime)}/${secondsToMinutesAndSeconds(currentSong.duration)}`
+            document.querySelector(".circle").style.left=(currentSong.currentTime/currentSong.duration)*100+"%"
+        })   
+    // Add an event listener to seek bar
+    
+        document.querySelector(".seekBar").addEventListener("click",e=>{
+            let percent=(e.offsetX/e.target.getBoundingClientRect().width)*100
+            document.querySelector(".circle").style.left=percent+"%";
+            currentSong.currentTime=(currentSong.duration*percent)/100
+        })
+    //add event listener for hamburger
+    document.querySelector(".hamburger").addEventListener("click",()=>{
+        document.querySelector(".left").style.left=0
+    })
+    //add event listener for close button 
+    document.querySelector(".close").addEventListener("click",()=>{
+        document.querySelector(".left").style.left=-120+"%"
     })
 }
 main();
